229106b4ff3bcbdc79d1c36cc05121f18714f5be
Emma Veldhuis <771511@apps.district196.org>
2020-04-28 11:34:18 -0500

    created outlets for all textFeilds on stats page

--- Mastermind_JAE2/Mastermind_JAE2/StatsViewController.swift
+++ Mastermind_JAE2/Mastermind_JAE2/StatsViewController.swift
@@ -13,6 +13,26 @@ class StatsViewController: UIViewController {
     
     var playingTeam = teamClass (name: "xxxxxx")
     
+    //outlets for all textfeilds on the stats page
+    @IBOutlet weak var firstPlace: UITextField!
+    @IBOutlet weak var secondPlace: UITextField!
+    @IBOutlet weak var thirdPlace: UITextField!
+    @IBOutlet weak var fourthPlace: UITextField!
+    @IBOutlet weak var fifthPlace: UITextField!
+    @IBOutlet weak var sixthPlace: UITextField!
+    @IBOutlet weak var seventhPlace: UITextField!
+    @IBOutlet weak var oneTry: UITextField!
+    @IBOutlet weak var twoTries: UITextField!
+    @IBOutlet weak var threeTries: UITextField!
+    @IBOutlet weak var fourTries: UITextField!
+    @IBOutlet weak var fiveTries: UITextField!
+    @IBOutlet weak var sixTries: UITextField!
+    @IBOutlet weak var sevenTries: UITextField!
+    @IBOutlet weak var eightTries: UITextField!
+    @IBOutlet weak var gamesLost: UITextField!
+    @IBOutlet weak var average: UITextField!
+    @IBOutlet weak var totalGamesPlayed: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
